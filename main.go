3a9a656b2ccf0dd8dbc65a3979a731d00e38d009
shrank <info@murxs.ch>
2025-08-01 13:59:33 +0200

    removed old code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"time"
   "github.com/julienschmidt/httprouter"
-  utils "msa/io-controller/utils"
 	api_class "msa/io-controller/api"
 )
 
@@ -14,35 +13,20 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   fmt.Fprint(w, "Welcome!\n")
 }
 
-// UNUSED allows unused variables to be included in Go programs
-func UNUSED(x ...interface{}) {}
-
 func main() {
 
-	MsgQueue := utils.CreateQueue()   // WebSocket message queue
-	UNUSED(MsgQueue)
   router := httprouter.New()
 	api := api_class.NewAPI()
-  // static files
+
+	// Index Page
+  router.GET("/",Index)
+
+	// static files
   router.ServeFiles("/debug/*filepath", http.Dir("./public"))
 	
   // Websockets
   router.GET("/api/v1/live", api.WsConnect)
-
-  // router.GET("/api/v1/", A.Auth(Index, dal.PERM_READ))
-
   
-  
-  // @TD: this is not nice, the event bus should be able to handle that somehow
-  // go func() {
-  //   current_message, _, _ :=MsgQueue.Last()
-  
-  //   for {
-  //     cnt, res, _ := MsgQueue.Wait(current_message)
-	// 		msg, _ := res.(api.WebsocketUpdate)
-  //     current_message = cnt
-  //   }
-  // }()
 
 	// Do your work here
   go func() {
