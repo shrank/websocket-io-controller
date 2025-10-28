6ed9caa50c272f3858a71c45a5d8c068fd05bf3e
shrank <info@murxs.ch>
2025-10-28 10:01:53 +0100

    set subdirectory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/debug/', // Change 'subdirectory' to match your subdirectory name
   server: {
     proxy: {
       '/api': {
