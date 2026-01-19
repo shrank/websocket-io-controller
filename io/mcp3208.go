4a6febb6bf0a199ecaf8c6a01cf43c620242c97b
shrank <info@murxs.ch>
2026-01-19 22:09:21 +0700

    syntax

--- io/mcp3208.go
+++ io/mcp3208.go
@@ -7,13 +7,12 @@ import (
 )
 
 var mcp3208_drivers = make(map[byte](*spi.MCP3208Driver))
-}
 
 func MCP3208_init(data *Card)(*Card) {
 	data.AddrCount=8
 	data.WordSize=1
 	data.ReadEvery=10
-	mcp3208_drivers[data.BusAddr] = spi.NewMCP3208Driver(board, spi.WithChipNumber(data.BusAddr-20))
+	mcp3208_drivers[data.BusAddr] = spi.NewMCP3208Driver(board, spi.WithChipNumber(int(data.BusAddr)-20))
 	err := mcp3208_drivers[data.BusAddr].Start()
 	if(err != nil) {
 		data.Status=err.Error()
@@ -29,7 +28,7 @@ func MCP3208_init(data *Card)(*Card) {
 func MCP3208_read(data *Card)(res []uint8, err error) {
 	res = make([]uint8, 8)
 	for k, _ := range res {
-		r, err := MCP3208_read_one(data, string(k)
+		r, err := MCP3208_read_one(data, k)
 		if(err != nil) {
 			return res, err
 		}
@@ -41,7 +40,7 @@ func MCP3208_read(data *Card)(res []uint8, err error) {
 func MCP3208_read_one(data *Card, channel int)(res uint8, err error) {
 	r, err := mcp3208_drivers[data.BusAddr].AnalogRead(string(channel))
 	if(err != nil) {
-		return r, err
+		return byte(r), err
 	}
 	return byte(r/4), nil
 }
