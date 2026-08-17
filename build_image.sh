734a585bc3b756ce49f3590524e289b00a52b442
shrank <info@murxs.ch>
2026-08-17 21:34:53 +0200

    fixed variable

--- build_image.sh
+++ build_image.sh
@@ -112,7 +112,7 @@ if [ -n "${TC_IMAGE}" ] && [ -f "${TC_IMAGE}" ]; then
 
     sudo cp "${CWD}/build/${OVERLAY_OUT}" "${MOUNT_POINT}/${OVERLAY_OUT}"
 
-    sudo sed -i -e 's/\.gz /.gz,${OVERLAY_OUT} /g' ${MOUNT_POINT}/config.txt
+    sudo sed -i -e "s/\.gz /.gz,${OVERLAY_OUT} /g" "${MOUNT_POINT}/config.txt"
     sudo sh -c "echo dtparam=i2c_arm=on >> ${MOUNT_POINT}/config.txt"
     sudo sh -c "echo dtparam=spi=on >> ${MOUNT_POINT}/config.txt"
 
